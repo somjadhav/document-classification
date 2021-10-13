80ef3583c0ce7977987d688421d7cffbda69b10b
Som Jadhav <somjadhav787@gmail.com>
2021-10-13 18:36:07 -0500

    changing h5 file parsing

--- Data/ST_hdf5_dataset_creation.py
+++ Data/ST_hdf5_dataset_creation.py
@@ -144,7 +144,7 @@ with h5py.File(hdf5_path, mode='w') as hdf5_file:
 
 def extract_ocrs(ocrs):
     
-    with h5py.File(hdf5_path, mode='a+') as hdf5_file:
+    with h5py.File(hdf5_path, mode='a') as hdf5_file:
         for i in range(len(ocrs)):
             # print how many images are saved every 1000 images
             if i % 100 == 0 and i > 1:
@@ -174,64 +174,64 @@ extract_ocrs(test_ocrs)
 
 
 
+with h5py.File(hdf5_path, mode='a') as hdf5_file:
+    # a numpy array to save the mean of the images
+    mean = np.zeros(train_shape[1:], np.float32)
+    # loop over train addresses
+    for i in range(len(train_addrs)):
+        # print how many images are saved every 1000 images
+        if i % 100 == 0 and i > 1:
+            print('Train data: {}/{}'.format(i, len(train_addrs)))
+        # read an image and resize to (img_size, img_size)
+        # cv2 load images as BGR, convert it to RGB
 
-# a numpy array to save the mean of the images
-mean = np.zeros(train_shape[1:], np.float32)
-# loop over train addresses
-for i in range(len(train_addrs)):
-    # print how many images are saved every 1000 images
-    if i % 100 == 0 and i > 1:
-        print('Train data: {}/{}'.format(i, len(train_addrs)))
-    # read an image and resize to (img_size, img_size)
-    # cv2 load images as BGR, convert it to RGB
-
-    addr = train_addrs[i]
-    img = cv2.imread(addr)
-    img = cv2.resize(img, (img_size, img_size), interpolation=cv2.INTER_CUBIC)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    # add any image pre-processing here
-    # # if the data order is Theano, axis orders should change
-    # if data_order == 'th':
-    #     img = np.rollaxis(img, 2)
-    # save the image and calculate the mean so far
-    hdf5_file["train_img"][i, ...] = img[None]
-    mean += img / float(len(train_labels))
+        addr = train_addrs[i]
+        img = cv2.imread(addr)
+        img = cv2.resize(img, (img_size, img_size), interpolation=cv2.INTER_CUBIC)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        # add any image pre-processing here
+        # # if the data order is Theano, axis orders should change
+        # if data_order == 'th':
+        #     img = np.rollaxis(img, 2)
+        # save the image and calculate the mean so far
+        hdf5_file["train_img"][i, ...] = img[None]
+        mean += img / float(len(train_labels))
 
 
-    # loop over validation addresses
-for i in range(len(val_addrs)):
-    # print how many images are saved every 1000 images
-    if i % 100 == 0 and i > 1:
-        print('Validation data: {}/{}'.format(i, len(val_addrs)))
-    # read an image and resize to (img_size, img_size)
-    # cv2 load images as BGR, convert it to RGB
-    addr = val_addrs[i]
-    img = cv2.imread(addr)
-    img = cv2.resize(img, (img_size, img_size), interpolation=cv2.INTER_CUBIC)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    # add any image pre-processing here
-    # # if the data order is Theano, axis orders should change
-    # if data_order == 'th':
-    #     img = np.rollaxis(img, 2)
-    # save the image
-    hdf5_file["val_img"][i, ...] = img[None]
-# loop over test addresses
-for i in range(len(test_addrs)):
-    # print how many images are saved every 1000 images
-    if i % 100 == 0 and i > 1:
-        print('Test data: {}/{}'.format(i, len(test_addrs)))
-    # read an image and resize to (img_size, img_size)
-    # cv2 load images as BGR, convert it to RGB
-    addr = test_addrs[i]
-    img = cv2.imread(addr)
-    img = cv2.resize(img, (img_size, img_size), interpolation=cv2.INTER_CUBIC)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    # add any image pre-processing here
-    # # if the data order is Theano, axis orders should change
-    # if data_order == 'th':
-    #     img = np.rollaxis(img, 2)
-    # save the image
-    hdf5_file["test_img"][i, ...] = img[None]
-# save the mean and close the hdf5 file
-hdf5_file["train_mean"][...] = mean
+        # loop over validation addresses
+    for i in range(len(val_addrs)):
+        # print how many images are saved every 1000 images
+        if i % 100 == 0 and i > 1:
+            print('Validation data: {}/{}'.format(i, len(val_addrs)))
+        # read an image and resize to (img_size, img_size)
+        # cv2 load images as BGR, convert it to RGB
+        addr = val_addrs[i]
+        img = cv2.imread(addr)
+        img = cv2.resize(img, (img_size, img_size), interpolation=cv2.INTER_CUBIC)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        # add any image pre-processing here
+        # # if the data order is Theano, axis orders should change
+        # if data_order == 'th':
+        #     img = np.rollaxis(img, 2)
+        # save the image
+        hdf5_file["val_img"][i, ...] = img[None]
+    # loop over test addresses
+    for i in range(len(test_addrs)):
+        # print how many images are saved every 1000 images
+        if i % 100 == 0 and i > 1:
+            print('Test data: {}/{}'.format(i, len(test_addrs)))
+        # read an image and resize to (img_size, img_size)
+        # cv2 load images as BGR, convert it to RGB
+        addr = test_addrs[i]
+        img = cv2.imread(addr)
+        img = cv2.resize(img, (img_size, img_size), interpolation=cv2.INTER_CUBIC)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        # add any image pre-processing here
+        # # if the data order is Theano, axis orders should change
+        # if data_order == 'th':
+        #     img = np.rollaxis(img, 2)
+        # save the image
+        hdf5_file["test_img"][i, ...] = img[None]
+    # save the mean and close the hdf5 file
+    hdf5_file["train_mean"][...] = mean
 hdf5_file.close()
